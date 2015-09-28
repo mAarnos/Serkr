ec47078c2fd916d480b84289991ec659613f4861
Mikko Aarnos <mikko.aarnos@gmail.com>
2015-09-28 19:56:53 +0300

    Fixed the test fail as Travis CI detected it.

--- src/cnf/simplify.rs
+++ src/cnf/simplify.rs
@@ -106,7 +106,7 @@ mod test {
     
     #[test]
     fn simplify_not_1() {
-        assert_eq!(simplify_not(Formula::False), Formula::False);
+        assert_eq!(simplify_not(Formula::False), Formula::True);
     }
     
     #[test]
