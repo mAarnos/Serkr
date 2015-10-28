05f498ccc7bdecdb24de08c23ebbe786e0c2e912
Mikko Aarnos <mikko.aarnos@gmail.com>
2015-10-28 19:03:17 +0200

    flatten_cnf is almost done.

--- src/prover/flatten_cnf.rs
+++ src/prover/flatten_cnf.rs
@@ -21,41 +21,39 @@ use utils::formula::{Term, Formula};
 use std::collections::HashMap;
 
 /// Turns a formula in CNF into the flat representation more suited for the prover.
-pub fn flatten_cnf_formula(_: Formula) -> Vec<Clause> {
-    Vec::new()
+/// We assume that the trivial cases of True and False have been handled already.
+pub fn flatten_cnf_formula(f: Formula) -> Vec<Clause> {
+    let mut mapping = HashMap::<String, i64>::new();
+    let mut n = 0;
+    collect(f, &mut mapping, &mut n)
 }
 
-/*
-fn collect(f: Formula) -> Vec<Clause> {
+fn collect(f: Formula, mapping: &mut HashMap<String, i64>, n: &mut i64) -> Vec<Clause> {
     match f {
-        Formula::Predicate(_, _) |
-        Formula::Not(box Formula::Predicate(_, _)) => vec!(vec!(f)),
-        Formula::Or(_, _) => vec!(collect_or(f)),
-        Formula::And(box p, box q) => { let mut left = collect(p); left.append(&mut collect(q)); left }
+        Formula::Predicate(s, args) => vec!(Clause::new_from_vec(vec!(create_literal(s, args, mapping, n)))),
+        Formula::Not(box Formula::Predicate(ref s, ref args)) => vec!(Clause::new_from_vec(vec!(create_literal(s.clone(), args.clone(), mapping, n).negate()))),
+        Formula::Or(_, _) => vec!(collect_or(f, mapping, n)),
+        Formula::And(box p, box q) => { let mut left = collect(p, mapping, n); left.append(&mut collect(q, mapping, n)); left }
         _ => panic!("The CNF transformation failed due to some kind of a bug")
     }
 }
-*/
 
-/*
 fn collect_or(f: Formula, mapping: &mut HashMap<String, i64>, n: &mut i64) -> Clause {
     match f {
-        Formula::Predicate(s, args) => Clause::new_from_vec(vec!(f)),
-        Formula::Not(box Formula::Predicate(s, args)) => Clause::new_from_vec(vec!(f)),
-        Formula::Or(box p, box q) => { let mut left = collect_or(p); left.add_literals(collect_or(q)); left }
+        Formula::Predicate(s, args) => Clause::new_from_vec(vec!(create_literal(s, args, mapping, n))),
+        Formula::Not(box Formula::Predicate(ref s, ref args)) => Clause::new_from_vec(vec!(create_literal(s.clone(), args.clone(), mapping, n).negate())),
+        Formula::Or(box p, box q) => { let mut left = collect_or(p, mapping, n); left.add_literals(collect_or(q, mapping, n)); left }
         _ => panic!("The CNF transformation failed due to some kind of a bug")
     }
 }
-*/
 
 fn create_literal(s: String, args: Vec<Term>, mapping: &mut HashMap<String, i64>, n: &mut i64) -> Literal {
     if let Some(&id) = mapping.get(&s) {
         Literal::new_from_id_and_args(id, args)
     } else {
-        let ret = Literal::new_from_id_and_args(*n, args);
-        mapping.insert(s, *n);
         *n += 1;
-        ret
+        mapping.insert(s, *n);
+        Literal::new_from_id_and_args(*n, args)
     }
 }
 
