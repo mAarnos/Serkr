ce04efc152564024e05fecbe38b740a1387075de
Mikko Aarnos <mikko.aarnos@gmail.com>
2015-11-17 00:10:12 +0200

    Added tests for delete_duplicates

--- src/prover/literal_deletion.rs
+++ src/prover/literal_deletion.rs
@@ -15,6 +15,7 @@
     along with Serkr. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use prover::literal::terms_equal;
 use prover::clause::Clause;
 
 /// Simplifies a clause if possible.
@@ -31,7 +32,7 @@ fn delete_duplicates(cl: &mut Clause) {
     while i < cl.size() {
         let mut j = i + 1;
         while j < cl.size() {
-            if cl[i] == cl[j] {
+            if terms_equal(&cl[i], &cl[j]) {
                 cl.swap_remove(j);
                 continue;
             }
@@ -56,7 +57,7 @@ fn delete_resolved(cl: &mut Clause) {
 
 #[cfg(test)]
 mod test {
-    use super::delete_resolved;
+    use super::{simplify, delete_resolved, delete_duplicates};
     use prover::term::Term;
     use prover::literal::Literal;
     use prover::clause::Clause;
@@ -73,6 +74,7 @@ mod test {
         let mut cl = Clause::new(vec!(l1.clone(), l2.clone(), l3.clone(), l4.clone()));
         
         delete_resolved(&mut cl);
+        assert_eq!(cl.size(), 3);
         assert!(cl.iter().any(|l| *l == l1));
         assert!(cl.iter().any(|l| *l == l2));
         assert!(!cl.iter().any(|l| *l == l3));
@@ -101,6 +103,74 @@ mod test {
           
         delete_resolved(&mut cl);
         assert!(cl.is_empty());
+    }
+    
+    #[test]
+    fn delete_duplicates_1() {
+        let x = Term::new(-1, false, Vec::new());
+        let y = Term::new(-2, false, Vec::new());
+        let z = Term::new(-3, false, Vec::new());
+        let l1 = Literal::new(true, x.clone(), y.clone());
+        let l2 = Literal::new(true, z.clone(), x.clone());
+        let l3 = Literal::new(false, y, x);
+        let mut cl = Clause::new(vec!(l1.clone(), l2.clone(), l3.clone()));
+
+        delete_duplicates(&mut cl);
+        
+        assert_eq!(cl.size(), 2);
+        assert!(cl.iter().any(|l| *l == l1));
+        assert!(cl.iter().any(|l| *l == l2));
+        assert!(!cl.iter().any(|l| *l == l3));
+    }
+    
+    #[test]
+    fn delete_duplicates_2() {
+        let x = Term::new(-1, false, Vec::new());
+        let y = Term::new(-2, false, Vec::new());
+        let z = Term::new(-3, false, Vec::new());
+        let w = Term::new(-4, false, Vec::new());
+        let l1 = Literal::new(true, x.clone(), y.clone());
+        let l2 = Literal::new(true, z.clone(), w.clone());
+        let l3 = Literal::new(false, w.clone(), x.clone());
+        let l4 = Literal::new(false, y, z);
+        let mut cl = Clause::new(vec!(l1, l2, l3, l4));
+        let cl_copy = cl.clone();
+        
+        delete_duplicates(&mut cl);
+        assert!(cl == cl_copy);
+    }
+    
+    #[test]
+    fn delete_duplicates_3() {
+        let x = Term::new(-1, false, Vec::new());
+        let l = Literal::new(false, x.clone(), x);
+        let mut cl = Clause::new(vec!(l.clone(), l.clone(), l.clone()));
+          
+        delete_duplicates(&mut cl);
+        assert_eq!(cl.size(), 1);
+        assert!(cl[0] == l);
+    }
+    
+    #[test]
+    fn simplify_1() {
+        let x = Term::new(-1, false, Vec::new());
+        let y = Term::new(-2, false, Vec::new());
+        let z = Term::new(-3, false, Vec::new());
+        let w = Term::new(-4, false, Vec::new());
+        let l1 = Literal::new(true, y.clone(), x.clone());
+        let l2 = Literal::new(false, w.clone(), z.clone());
+        let l3 = Literal::new(true, w.clone(), w.clone());
+        let l4 = Literal::new(false, x, y);
+        let l5 = Literal::new(false, z, w);
+        let mut cl = Clause::new(vec!(l1.clone(), l2.clone(), l3.clone(), l4.clone(), l5.clone()));
+        
+        simplify(&mut cl);
+        assert_eq!(cl.size(), 2);
+        assert!(cl.iter().any(|l| *l == l1));
+        assert!(cl.iter().any(|l| *l == l2));
+        assert!(!cl.iter().any(|l| *l == l3));
+        assert!(!cl.iter().any(|l| *l == l4));
+        assert!(!cl.iter().any(|l| *l == l5));
     }
 } 
 
