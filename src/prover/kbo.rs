c9e0b5d90aae66754e3c9024cec994f2835b3896
Mikko Aarnos <mikko.aarnos@gmail.com>
2015-12-03 18:54:29 +0200

    KBO is probably finished.

--- src/prover/kbo.rs
+++ src/prover/kbo.rs
@@ -15,6 +15,7 @@
     along with Serkr. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use std::collections::hash_map::HashMap;
 use prover::term::Term;
 use prover::term_ordering::TermOrdering;
 
@@ -89,10 +90,10 @@ impl TermOrdering for KBO {
             let s_weight = self.weight(s);
             let t_weight = self.weight(t);
             if s_weight > t_weight {
-                unimplemented!();
+                variable_domination(s, t)
             } else if s_weight == t_weight {
                 if self.precedence(s, t) || (s.get_id() == t.get_id() && self.lexical_ordering(s, t)) {
-                    unimplemented!();
+                    variable_domination(s, t)
                 } else {
                     false
                 }
@@ -108,6 +109,28 @@ impl TermOrdering for KBO {
     
     fn ge(&self, s: &Term, t: &Term) -> bool {
         s == t || self.gt(s, t)
+    }
+}
+
+fn variable_domination(s: &Term, t: &Term) -> bool {
+    let mut variable_counts = HashMap::new();
+    variable_count(&mut variable_counts, s, 1);
+    variable_count(&mut variable_counts, t, -1);
+    variable_counts.values().all(|&count| count >= 0)
+}
+
+fn variable_count(counts: &mut HashMap<i64, i64>, t: &Term, weight: i64) {
+    if t.is_variable() {
+        // NOTE: suffers from if let borrow bug.
+        if let Some(v) = counts.get_mut(&t.get_id()) {
+            *v += weight;
+            return;
+        }
+        counts.insert(t.get_id(), weight);
+    } else {
+        for x in t.iter() {
+            variable_count(counts, x, weight);
+        }
     }
 }
 
