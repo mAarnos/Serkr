d520e1cc4deb34206cf7dbe522ed2162aeed8603
Mikko Aarnos <mikko.aarnos@gmail.com>
2016-03-06 23:36:19 +0200

    Ran rustfmt on parser/internal_parser

--- src/parser/internal_parser/ast.rs
+++ src/parser/internal_parser/ast.rs
@@ -1,29 +1,28 @@
-/*
-    Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
+// Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
+//
+// Serkr is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Serkr is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Serkr. If not, see <http://www.gnu.org/licenses/>.
+//
 
-    Serkr is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Serkr is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Serkr. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/// Data type for FOL terms. 
+/// Data type for FOL terms.
 #[derive(Debug, Eq, PartialEq, Clone, Hash)]
 #[allow(missing_docs)]
 pub enum Term {
     Variable(String),
-    Function(String, Vec<Term>)
+    Function(String, Vec<Term>),
 }
 
-/// Data type for FOL formulas. 
+/// Data type for FOL formulas.
 #[derive(Debug, Eq, PartialEq, Clone, Hash)]
 #[allow(missing_docs)]
 pub enum Formula {
