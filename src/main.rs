0f061d3728f3d1fd26e09232747c5d93368bdbb8
Mikko Aarnos <mikko.aarnos@gmail.com>
2016-03-07 00:44:31 +0200

    Only CNF transformer left to rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-/*
-    Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
-
-    Serkr is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Serkr is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Serkr. If not, see <http://www.gnu.org/licenses/>.
-*/
+// Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
+//
+// Serkr is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Serkr is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Serkr. If not, see <http://www.gnu.org/licenses/>.
+//
 
 //! Serkr is an automated theorem prover for first order logic.
 
@@ -22,9 +21,9 @@
         unused_extern_crates,
         variant_size_differences,
         missing_docs,
-        missing_debug_implementations, 
+        missing_debug_implementations,
         missing_copy_implementations,
-        trivial_casts, 
+        trivial_casts,
         trivial_numeric_casts,
         unsafe_code,
         unused_import_braces,
@@ -46,37 +45,43 @@ extern crate num;
 #[macro_use]
 pub mod utils;
 pub mod parser;
+#[cfg_attr(rustfmt, rustfmt_skip)]
 pub mod cnf;
 pub mod prover;
 
 fn main() {
     let matches = clap::App::new("Serkr")
-                                .version(crate_version!())
-                                .author("Mikko Aarnos <mikko.aarnos@gmail.com>")
-                                .about("An automated theorem prover for first order logic with equality")
-                                .args_from_usage("<INPUT> 'The TPTP file the program should analyze'
-                                                  -t, --time_limit=[time_limit] 'Time limit for the prover (default=300s)'")
-                                .arg(clap::Arg::with_name("lpo")
-                                    .help("Use LPO as the term ordering") 
-                                    .short("l") 
-                                    .long("lpo") 
-                                    .conflicts_with("kbo"))
-                                .arg(clap::Arg::with_name("kbo")
-                                    .help("Use KBO as the term ordering (default)") 
-                                    .short("k") 
-                                    .long("kbo") 
-                                    .conflicts_with("lpo"))
-                                .get_matches();
-    
+                      .version(crate_version!())
+                      .author("Mikko Aarnos <mikko.aarnos@gmail.com>")
+                      .about("An automated theorem prover for first order logic with equality")
+                      .args_from_usage("<INPUT> 'The TPTP file the program should analyze'
+                                        -t, --time_limit=[time_limit] 'Time limit for the prover
+                                        (default=300s)'")
+                      .arg(clap::Arg::with_name("lpo")
+                               .help("Use LPO as the term ordering")
+                               .short("l")
+                               .long("lpo")
+                               .conflicts_with("kbo"))
+                      .arg(clap::Arg::with_name("kbo")
+                               .help("Use KBO as the term ordering (default)")
+                               .short("k")
+                               .long("kbo")
+                               .conflicts_with("lpo"))
+                      .get_matches();
+
     println!("Serkr {}, (C) 2015-2016 Mikko Aarnos", crate_version!());
-                 
-    // Hack to get around the parser/CNF transformer from crashing due to stack overflow when analyzing very large files.            
-    let child = std::thread::Builder::new().stack_size(32 * 1024 * 1024).spawn(move || {
-        let input_file = matches.value_of("INPUT").expect("This should always be OK");
-        let use_lpo = matches.is_present("lpo");
-        let time_limit = value_t!(matches, "time_limit", u64).unwrap_or(300);
-        prover::proof_search::prove(&input_file, use_lpo, time_limit)
-    }).unwrap();
+
+    // Hack to get around the parser/CNF transformer from crashing with very large files.
+    let child = std::thread::Builder::new()
+                    .stack_size(32 * 1024 * 1024)
+                    .spawn(move || {
+                        let input_file = matches.value_of("INPUT")
+                                                .expect("This should always be OK");
+                        let use_lpo = matches.is_present("lpo");
+                        let time_limit = value_t!(matches, "time_limit", u64).unwrap_or(300);
+                        prover::proof_search::prove(&input_file, use_lpo, time_limit)
+                    })
+                    .unwrap();
 
     let (proof_result, proof_statistics) = child.join().unwrap();
 
