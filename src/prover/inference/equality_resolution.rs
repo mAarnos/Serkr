5c662d35ab9710b4d55e172b8da987897a51b3cb
Mikko Aarnos <mikko.aarnos@gmail.com>
2016-03-06 16:51:56 +0200

    Ran rustfmt on prover/inference

--- src/prover/inference/equality_resolution.rs
+++ src/prover/inference/equality_resolution.rs
@@ -1,19 +1,18 @@
-/*
-    Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
-
-    Serkr is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Serkr is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Serkr. If not, see <http://www.gnu.org/licenses/>.
-*/
+// Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
+//
+// Serkr is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Serkr is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Serkr. If not, see <http://www.gnu.org/licenses/>.
+//
 
 use prover::data_structures::clause::Clause;
 use prover::unification::full_unification::mgu;
@@ -22,9 +21,12 @@ use prover::inference::maximality::literal_maximal_in;
 
 /// Infers new clauses by (ordered) equality resolution.
 /// Returns the amount of inferred clauses.
-pub fn equality_resolution(term_ordering: &TermOrdering, cl: &Clause, generated: &mut Vec<Clause>) -> u64 {
+pub fn equality_resolution(term_ordering: &TermOrdering,
+                           cl: &Clause,
+                           generated: &mut Vec<Clause>)
+                           -> u64 {
     let mut er_count = 0;
-    
+
     for (i, l) in cl.iter().enumerate() {
         if l.is_negative() {
             if let Some(sigma) = mgu(l.get_lhs(), l.get_rhs()) {
@@ -33,21 +35,19 @@ pub fn equality_resolution(term_ordering: &TermOrdering, cl: &Clause, generated:
                 new_cl.swap_remove(i);
                 new_cl.subst(&sigma);
                 new_l.subst(&sigma);
-                
+
                 assert_eq!(new_l.get_lhs(), new_l.get_rhs());
-                
+
                 if literal_maximal_in(term_ordering, &new_cl, &new_l) {
                     generated.push(new_cl);
                     er_count += 1;
-                } 
+                }
             }
         }
     }
-    
+
     er_count
-} 
+}
 
 #[cfg(test)]
-mod test {
-    
-} 
+mod test {}
--- src/prover/inference/superposition.rs
+++ src/prover/inference/superposition.rs
@@ -1,19 +1,18 @@
-/*
-    Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
-
-    Serkr is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Serkr is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Serkr. If not, see <http://www.gnu.org/licenses/>.
-*/
+// Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
+//
+// Serkr is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Serkr is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Serkr. If not, see <http://www.gnu.org/licenses/>.
+//
 
 // TODO: clean this crap up.
 
@@ -39,40 +38,47 @@ fn create_overlapped_term0(u_p: &mut Term, t: &Term, trace: &[usize], n: usize) 
     }
 }
 
-fn overlaps(term_ordering: &TermOrdering, 
-            s: &Term, t: &Term, 
-            u: &Term, v: &Term, u_v_negated: bool,
+/// TODO: I really, REALLY should document and clean this up.
+fn overlaps(term_ordering: &TermOrdering,
+            s: &Term,
+            t: &Term,
+            u: &Term,
+            v: &Term,
+            u_v_negated: bool,
             u_p: &Term,
-            cl1: &Clause, cl1_i: usize, 
-            cl2: &Clause,  cl2_i: usize, 
+            cl1: &Clause,
+            cl1_i: usize,
+            cl2: &Clause,
+            cl2_i: usize,
             trace: &mut Vec<usize>,
-            generated: &mut Vec<Clause>) -> u64 {
+            generated: &mut Vec<Clause>)
+            -> u64 {
     let mut sp_count = 0;
-    
+
     if !u_p.is_variable() {
-        if let Some(sigma) = mgu(u_p, s) {            
+        if let Some(sigma) = mgu(u_p, s) {
             let mut new_s_t = Literal::new(false, s.clone(), t.clone());
-            new_s_t.subst(&sigma);      
-            
+            new_s_t.subst(&sigma);
+
             if !term_ordering.gt(new_s_t.get_rhs(), new_s_t.get_lhs()) {
                 let mut new_u_v = Literal::new(u_v_negated, u.clone(), v.clone());
-                new_u_v.subst(&sigma); 
-                
+                new_u_v.subst(&sigma);
+
                 if !term_ordering.gt(new_u_v.get_rhs(), new_u_v.get_lhs()) {
                     let mut new_c = cl1.clone();
                     new_c.swap_remove(cl1_i);
                     new_c.subst(&sigma);
-                    
-                    if literal_strictly_maximal_in(term_ordering, &new_c, &new_s_t) {                                                      
+
+                    if literal_strictly_maximal_in(term_ordering, &new_c, &new_s_t) {
                         let mut new_d = cl2.clone();
                         new_d.swap_remove(cl2_i);
                         new_d.subst(&sigma);
-                        
-                        let maximality_condition_fulfilled = if u_v_negated { 
-                                                                literal_maximal_in(term_ordering, &new_d, &new_u_v)
-                                                             } else { 
-                                                                literal_strictly_maximal_in(term_ordering, &new_d, &new_u_v)
-                                                             };
+
+                        let maximality_condition_fulfilled = if u_v_negated {
+                            literal_maximal_in(term_ordering, &new_d, &new_u_v)
+                        } else {
+                            literal_strictly_maximal_in(term_ordering, &new_d, &new_u_v)
+                        };
                         if maximality_condition_fulfilled {
                             let new_u = create_overlapped_term(u, t, trace);
                             let mut new_lit = Literal::new(u_v_negated, new_u, v.clone());
@@ -84,23 +90,38 @@ fn overlaps(term_ordering: &TermOrdering,
                         }
                     }
                 }
-            }            
+            }
         }
-        
+
         for (i, x) in u_p.iter().enumerate() {
             trace.push(i);
-            sp_count += overlaps(term_ordering, s, t, u, v, u_v_negated, x, cl1, cl1_i, cl2, cl2_i, trace, generated);
+            sp_count += overlaps(term_ordering,
+                                 s,
+                                 t,
+                                 u,
+                                 v,
+                                 u_v_negated,
+                                 x,
+                                 cl1,
+                                 cl1_i,
+                                 cl2,
+                                 cl2_i,
+                                 trace,
+                                 generated);
             trace.pop();
         }
     }
-    
+
     sp_count
 }
 
-fn overlaps_literal(term_ordering: &TermOrdering, 
-                    cl1: &Clause, cl1_i: usize, 
-                    cl2: &Clause, cl2_i: usize, 
-                    generated: &mut Vec<Clause>) -> u64 {
+fn overlaps_literal(term_ordering: &TermOrdering,
+                    cl1: &Clause,
+                    cl1_i: usize,
+                    cl2: &Clause,
+                    cl2_i: usize,
+                    generated: &mut Vec<Clause>)
+                    -> u64 {
     let mut trace = Vec::new();
     let l_lhs = cl1[cl1_i].get_lhs();
     let l_rhs = cl1[cl1_i].get_rhs();
@@ -108,24 +129,73 @@ fn overlaps_literal(term_ordering: &TermOrdering,
     let r_lhs = cl2[cl2_i].get_lhs();
     let r_rhs = cl2[cl2_i].get_rhs();
     let mut sp_count = 0;
-    
+
     // Four different ways to arrange two equations
-    sp_count += overlaps(term_ordering, l_lhs, l_rhs, r_lhs, r_rhs, r_negated, r_lhs, cl1, cl1_i, cl2, cl2_i, &mut trace, generated);
-    sp_count += overlaps(term_ordering, l_rhs, l_lhs, r_lhs, r_rhs, r_negated, r_lhs, cl1, cl1_i, cl2, cl2_i, &mut trace, generated);
-    sp_count += overlaps(term_ordering, l_lhs, l_rhs, r_rhs, r_lhs, r_negated, r_rhs, cl1, cl1_i, cl2, cl2_i, &mut trace, generated);
-    sp_count += overlaps(term_ordering, l_rhs, l_lhs, r_rhs, r_lhs, r_negated, r_rhs, cl1, cl1_i, cl2, cl2_i, &mut trace, generated);
-    
+    sp_count += overlaps(term_ordering,
+                         l_lhs,
+                         l_rhs,
+                         r_lhs,
+                         r_rhs,
+                         r_negated,
+                         r_lhs,
+                         cl1,
+                         cl1_i,
+                         cl2,
+                         cl2_i,
+                         &mut trace,
+                         generated);
+    sp_count += overlaps(term_ordering,
+                         l_rhs,
+                         l_lhs,
+                         r_lhs,
+                         r_rhs,
+                         r_negated,
+                         r_lhs,
+                         cl1,
+                         cl1_i,
+                         cl2,
+                         cl2_i,
+                         &mut trace,
+                         generated);
+    sp_count += overlaps(term_ordering,
+                         l_lhs,
+                         l_rhs,
+                         r_rhs,
+                         r_lhs,
+                         r_negated,
+                         r_rhs,
+                         cl1,
+                         cl1_i,
+                         cl2,
+                         cl2_i,
+                         &mut trace,
+                         generated);
+    sp_count += overlaps(term_ordering,
+                         l_rhs,
+                         l_lhs,
+                         r_rhs,
+                         r_lhs,
+                         r_negated,
+                         r_rhs,
+                         cl1,
+                         cl1_i,
+                         cl2,
+                         cl2_i,
+                         &mut trace,
+                         generated);
+
     sp_count
 }
 
 /// Generates superposition inferences between two clauses, in one direction.
 /// Returns the amount of generated clauses.
-fn superposition_generate(term_ordering: &TermOrdering, 
-                          cl1: &Clause, 
-                          cl2: &Clause, 
-                          generated: &mut Vec<Clause>) -> u64 {
+fn superposition_generate(term_ordering: &TermOrdering,
+                          cl1: &Clause,
+                          cl2: &Clause,
+                          generated: &mut Vec<Clause>)
+                          -> u64 {
     let mut sp_count = 0;
-    
+
     for (i, l1) in cl1.iter().enumerate() {
         if l1.is_positive() {
             for j in 0..cl2.size() {
@@ -133,7 +203,7 @@ fn superposition_generate(term_ordering: &TermOrdering,
             }
         }
     }
-    
+
     sp_count
 }
 
@@ -141,21 +211,20 @@ fn superposition_generate(term_ordering: &TermOrdering,
 /// Time complexity is who the fuck knows.
 /// Assumes that cl was renamed so that it has no variables in common with any other clause.
 /// Returns the amount of clauses generated.
-pub fn superposition(term_ordering: &TermOrdering, 
-                     cl: &Clause, 
-                     clauses: &[Clause], 
-                     generated: &mut Vec<Clause>) -> u64 {
+pub fn superposition(term_ordering: &TermOrdering,
+                     cl: &Clause,
+                     clauses: &[Clause],
+                     generated: &mut Vec<Clause>)
+                     -> u64 {
     let mut sp_count = 0;
-                                               
+
     for cl2 in clauses {
         sp_count += superposition_generate(term_ordering, cl, cl2, generated);
         sp_count += superposition_generate(term_ordering, cl2, cl, generated);
-    }    
+    }
 
     sp_count
 }
 
 #[cfg(test)]
-mod test {
-
-} 
+mod test {}
