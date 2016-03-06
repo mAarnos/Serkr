b969b872d0dcc5ba25634da55bec3bbf2c0fab17
Mikko Aarnos <mikko.aarnos@gmail.com>
2016-03-06 17:17:12 +0200

    Ran rustfmt on prover

--- src/prover/mod.rs
+++ src/prover/mod.rs
@@ -1,21 +1,20 @@
-/*
-    Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
+// Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
+//
+// Serkr is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Serkr is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Serkr. If not, see <http://www.gnu.org/licenses/>.
+//
 
-    Serkr is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Serkr is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Serkr. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-//! Contains the main proof search. 
+//! Contains the main proof search.
 
 /// Contains all the simplification rules used by the proof search.
 mod simplification;
@@ -36,7 +35,8 @@ mod data_structures;
 /// Contains functions for automatically determining good prover settings for a given problem.
 mod problem_analysis;
 
-// TODO: figure out a new name for this, flatten is just not descriptive and is also means something else on top of that.
+// TODO: figure out a new name for this.
+// flatten is just not descriptive and it also means something else on top of that.
 mod flatten_cnf;
 
 /// Contains stuff on statistics collected during a proof search.
--- src/prover/proof_state.rs
+++ src/prover/proof_state.rs
@@ -1,25 +1,25 @@
-/*
-    Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
-
-    Serkr is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Serkr is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Serkr. If not, see <http://www.gnu.org/licenses/>.
-*/
+// Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
+//
+// Serkr is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Serkr is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Serkr. If not, see <http://www.gnu.org/licenses/>.
+//
 
 use std::collections::BinaryHeap;
 use prover::data_structures::clause::Clause;
 use prover::ordering::term_ordering::TermOrdering;
 
-/// Contains the current proof state, that is, the used, the unused clauses and the term ordering used.
+/// Contains the current proof state.
+/// That is, the used, the unused clauses and the term ordering used.
 pub struct ProofState {
     used_clauses: Vec<Clause>,
     unused_clauses: BinaryHeap<Clause>,
@@ -27,54 +27,56 @@ pub struct ProofState {
 }
 
 impl ProofState {
-    /// Creates a new proof state with the used clauses empty, the preprocessed_clauses in unused and the given term ordering.
+    /// Creates a new proof state.
     pub fn new(preprocessed_clauses: Vec<Clause>, term_order: TermOrdering) -> ProofState {
-        ProofState { used_clauses: Vec::new(), 
-                     unused_clauses: preprocessed_clauses.into_iter().collect(), 
-                     term_ordering: term_order }
+        ProofState {
+            used_clauses: Vec::new(),
+            unused_clauses: preprocessed_clauses.into_iter().collect(),
+            term_ordering: term_order,
+        }
     }
-         
+
     /// Get the amount of used clauses.
     pub fn get_used_size(&self) -> usize {
         self.used_clauses.len()
     }
-    
+
     /// Get the amount of unused clauses.
     pub fn get_unused_size(&self) -> usize {
         self.unused_clauses.len()
     }
-    
+
     /// Picks the best clause from unused and removes it from there.
     pub fn pick_best_clause(&mut self) -> Option<Clause> {
         self.unused_clauses.pop()
     }
-          
+
     /// Adds the given clause to used clauses.
     pub fn add_to_used(&mut self, cl: Clause) {
         self.used_clauses.push(cl);
     }
-    
+
     /// Adds the given clause to unused clauses.
     pub fn add_to_unused(&mut self, cl: Clause) {
         self.unused_clauses.push(cl);
     }
-  
+
     /// Get a reference term ordering used.
     pub fn get_term_ordering(&self) -> &TermOrdering {
         &self.term_ordering
     }
-    
+
     /// Get a reference to the used clauses.
     pub fn get_used(&self) -> &Vec<Clause> {
         &self.used_clauses
     }
-    
+
     /// Get a reference to the unused clauses.
     #[allow(dead_code)]
     pub fn get_unused(&self) -> &BinaryHeap<Clause> {
         &self.unused_clauses
     }
-   
+
     /// Get a mutable reference to the used clauses.
     pub fn get_used_mut(&mut self) -> &mut Vec<Clause> {
         &mut self.used_clauses
