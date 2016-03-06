3131c0a35c10bf43cc2a657512efcbb665f8fd7f
Mikko Aarnos <mikko.aarnos@gmail.com>
2016-03-06 16:47:02 +0200

    Ran rustfmt on prover/simplification

--- src/prover/simplification/equality_subsumption.rs
+++ src/prover/simplification/equality_subsumption.rs
@@ -1,19 +1,18 @@
-/*
-    Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
-
-    Serkr is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Serkr is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Serkr. If not, see <http://www.gnu.org/licenses/>.
-*/
+// Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
+//
+// Serkr is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Serkr is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Serkr. If not, see <http://www.gnu.org/licenses/>.
+//
 
 use prover::data_structures::term::Term;
 use prover::data_structures::literal::Literal;
@@ -27,13 +26,13 @@ pub fn eqn_subsumes_eqn(s: &Term, t: &Term, u: &Term, v: &Term) -> bool {
         true
     } else if u.is_function() && u.get_id() == v.get_id() {
         assert_eq!(u.get_arity(), v.get_arity());
-            
+
         for i in 0..u.get_arity() {
             if !eqn_subsumes_eqn(s, t, &u[i], &v[i]) {
                 return false;
             }
         }
-            
+
         true
     } else {
         false
@@ -64,48 +63,49 @@ mod test {
     use prover::data_structures::term::Term;
     use prover::data_structures::literal::Literal;
     use prover::data_structures::clause::Clause;
-    
+
     #[test]
     fn equality_subsumes_clause_1() {
-        // Check that f(x) = x subsumes g(f(a), f(b)) = g(a, b). 
+        // Check that f(x) = x subsumes g(f(a), f(b)) = g(a, b).
         let x = Term::new_variable(-1);
         let a = Term::new_function(1, Vec::new());
-        let b = Term::new_function(2, Vec::new());  
-        let g_f_a_f_b = Term::new_function(4, vec!(Term::new_function(3, vec!(a.clone())), Term::new_function(3, vec!(b.clone()))));
-        let g_a_b = Term::new_function(4, vec!(a.clone(), b.clone()));
-        
-        let cl1 = Clause::new(vec!(Literal::new(false, Term::new_function(3, vec!(x.clone())), x)));
-        let cl2 = Clause::new(vec!(Literal::new(false, g_f_a_f_b, g_a_b)));
-    
+        let b = Term::new_function(2, Vec::new());
+        let g_f_a_f_b = Term::new_function(4,
+                                           vec![Term::new_function(3, vec![a.clone()]),
+                                                Term::new_function(3, vec![b.clone()])]);
+        let g_a_b = Term::new_function(4, vec![a.clone(), b.clone()]);
+
+        let cl1 = Clause::new(vec![Literal::new(false, Term::new_function(3, vec![x.clone()]), x)]);
+        let cl2 = Clause::new(vec![Literal::new(false, g_f_a_f_b, g_a_b)]);
+
         assert!(equality_subsumes_clause(&cl1, &cl2));
     }
-    
+
     #[test]
     fn equality_subsumes_clause_2() {
-        // Check that f(x, c) = x subsumes g(y) = g(f(y, c)). 
+        // Check that f(x, c) = x subsumes g(y) = g(f(y, c)).
         let x = Term::new_variable(-1);
         let y = Term::new_variable(-2);
         let c = Term::new_function(1, Vec::new());
-        let f_x_c = Term::new_function(2, vec!(x.clone(), c.clone()));
-        let g_y = Term::new_function(3, vec!(y.clone()));
-        let g_f_y_c = Term::new_function(3, vec!(Term::new_function(2, vec!(y, c))));
-        
-        let cl1 = Clause::new(vec!(Literal::new(false, f_x_c, x)));
-        let cl2 = Clause::new(vec!(Literal::new(false, g_y, g_f_y_c)));
-    
+        let f_x_c = Term::new_function(2, vec![x.clone(), c.clone()]);
+        let g_y = Term::new_function(3, vec![y.clone()]);
+        let g_f_y_c = Term::new_function(3, vec![Term::new_function(2, vec![y, c])]);
+
+        let cl1 = Clause::new(vec![Literal::new(false, f_x_c, x)]);
+        let cl2 = Clause::new(vec![Literal::new(false, g_y, g_f_y_c)]);
+
         assert!(equality_subsumes_clause(&cl1, &cl2));
     }
-    
+
     #[test]
     fn equality_subsumes_clause_3() {
-        // Check that f(x) = T subsumes T = f(x). 
+        // Check that f(x) = T subsumes T = f(x).
         let x = Term::new_variable(-1);
-        let f_x = Term::new_function(1, vec!(x));
-        
-        let cl1 = Clause::new(vec!(Literal::new(false, f_x.clone(), Term::new_truth())));
-        let cl2 = Clause::new(vec!(Literal::new(false, Term::new_truth(), f_x.clone())));
-    
+        let f_x = Term::new_function(1, vec![x]);
+
+        let cl1 = Clause::new(vec![Literal::new(false, f_x.clone(), Term::new_truth())]);
+        let cl2 = Clause::new(vec![Literal::new(false, Term::new_truth(), f_x.clone())]);
+
         assert!(equality_subsumes_clause(&cl1, &cl2));
     }
-} 
-
+}
--- src/prover/simplification/simplify_reflect.rs
+++ src/prover/simplification/simplify_reflect.rs
@@ -1,19 +1,18 @@
-/*
-    Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
-
-    Serkr is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Serkr is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Serkr. If not, see <http://www.gnu.org/licenses/>.
-*/
+// Serkr - An automated theorem prover. Copyright (C) 2015-2016 Mikko Aarnos.
+//
+// Serkr is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Serkr is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Serkr. If not, see <http://www.gnu.org/licenses/>.
+//
 
 use prover::data_structures::term::Term;
 use prover::data_structures::clause::Clause;
@@ -35,13 +34,13 @@ pub fn simplify_reflect(cl1: &Clause, cl2: &mut Clause) {
 /// Simplifies the clause with positive simplify-reflect.
 fn positive_simplify_reflect(s: &Term, t: &Term, cl2: &mut Clause) {
     let mut i = 0;
-    
+
     while i < cl2.size() {
         if cl2[i].is_negative() && eqn_subsumes_eqn(s, t, cl2[i].get_lhs(), cl2[i].get_rhs()) {
             cl2.swap_remove(i);
             continue;
         }
-        
+
         i += 1;
     }
 }
@@ -49,19 +48,16 @@ fn positive_simplify_reflect(s: &Term, t: &Term, cl2: &mut Clause) {
 /// Simplifies the clause with negative simplify-reflect.
 fn negative_simplify_reflect(s: &Term, t: &Term, cl2: &mut Clause) {
     let mut i = 0;
-    
+
     while i < cl2.size() {
         if cl2[i].is_positive() && match_term_pairs(s, t, cl2[i].get_lhs(), cl2[i].get_rhs()) {
             cl2.swap_remove(i);
             continue;
         }
-        
+
         i += 1;
     }
 }
 
 #[cfg(test)]
-mod test {
-
-} 
-
+mod test {}
