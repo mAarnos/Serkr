eff603d1be7250ff72afbc5510207c05b7102efc
Mikko Aarnos <mikko.aarnos@gmail.com>
2016-03-15 13:46:24 +0200

    Added some tests for free_variables

--- src/cnf/free_variables.rs
+++ src/cnf/free_variables.rs
@@ -34,7 +34,7 @@ pub fn free_in(f: &Formula, t: &Term) -> bool {
 }
 
 /// Used for checking if a term s occurs in a term t.
-pub fn occurs_in(t: &Term, s: &Term) -> bool {
+fn occurs_in(t: &Term, s: &Term) -> bool {
     t == s ||
     match *t {
         Term::Variable(_) => false,
@@ -61,7 +61,7 @@ pub fn fv(f: &Formula) -> HashSet<i64> {
 }
 
 /// Get the free variables of a term.
-pub fn fvt(t: &Term) -> HashSet<i64> {
+fn fvt(t: &Term) -> HashSet<i64> {
     match *t {
         Term::Variable(id) => {
             assert!(id < 0);
@@ -74,4 +74,50 @@ pub fn fvt(t: &Term) -> HashSet<i64> {
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+    use super::{free_in, fv};
+    use cnf::ast::{Term,Formula};
+    
+    #[test]
+    fn free_in_1() {
+        let x = Term::Variable(-1);
+        let y = Term::Variable(-2);
+        let z = Term::Variable(-3);
+        let a = Term::Variable(-4); // not used in f
+        let p_x_y = Formula::Predicate(1, vec![x.clone(), y.clone()]);
+        let p_y_z = Formula::Predicate(1, vec![y.clone(), z.clone()]);
+        let p_x_z = Formula::Predicate(1, vec![x.clone(), z.clone()]);
+        let f_q = Formula::Implies(Box::new(Formula::And(vec![p_x_y, p_y_z])), Box::new(p_x_z));
+        let f = Formula::Forall(-1, Box::new(Formula::Forall(-2, Box::new(Formula::Forall(-3, Box::new(f_q))))));
+        
+        assert!(!free_in(&f, &x));
+        assert!(!free_in(&f, &y));
+        assert!(!free_in(&f, &z));
+        assert!(!free_in(&f, &a));
+    }
+    
+    #[test]
+    fn free_in_2() {
+        let x = Term::Variable(-1);
+        let y = Term::Variable(-2);
+        let f_y = Term::Function(1, vec![y.clone()]);
+        let p_x_f_y = Formula::Predicate(1, vec![x.clone(), f_y]);
+        let f = Formula::Forall(-1, Box::new(Formula::Exists(-3, Box::new(p_x_f_y))));
+        
+        assert!(!free_in(&f, &x));
+        assert!(free_in(&f, &y));
+    }
+    
+    #[test]
+    fn fv_1() {
+        let x = Term::Variable(-1);
+        let y = Term::Variable(-2);
+        let z = Term::Variable(-3);
+        let p_x_y_z = Formula::Predicate(1, vec![x.clone(), y.clone(), z.clone()]);
+        let f = Formula::Forall(-1, Box::new(Formula::Exists(-3, Box::new(p_x_y_z))));
+        let free_variables = fv(&f);
+        
+        assert_eq!(free_variables.len(), 1);
+        assert!(free_variables.contains(&-2));
+    }
+}
